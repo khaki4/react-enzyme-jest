082a64fa0a5fd38c9a4637171399c4be67f284bf
pi3rrot <4dakto@gmail.com>
2018-12-13 21:03:23 +0900

    set up enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'; 
 import App from './App';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
