5d4178cf40edf280cdc4501564c3f2f334b1d8ea
pi3rrot <4dakto@gmail.com>
2018-12-14 20:49:10 +0900

    Test React Component Props with Enzyme and Jest

--- src/App.js
+++ src/App.js
@@ -28,4 +28,10 @@ class App extends Component {
   }
 }
 
+export class Link extends Component {
+  render() {
+    return this.props.hide ? null : <a href={this.props.address}>Click</a>
+  }
+}
+
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'; 
 import toJson from 'enzyme-to-json';
-import App from './App';
+import App, { Link } from './App';
 
 configure({ adapter: new Adapter() });
 
@@ -19,5 +18,23 @@ describe('<App />', () => {
     const tree = shallow(<App />);
     expect(toJson(tree)).toMatchSnapshot()
   });
+});
 
+describe('<Link />', () => {
+  it('link component accepts address props', () => {
+    const wrapper = shallow(<Link address='www.google.com' />);
+    expect(wrapper.instance().props.address).toBe('www.google.com');
+  });
+
+  it('a tag node renders href correctly', () => {
+    const wrapper = shallow(<Link address='www.google.com' />);
+    expect(wrapper.props().href).toBe('www.google.com');
+  });
+
+  it('returns null with true hide prop', () => {
+    const wrapper = shallow(<Link hide={false} /> );
+    expect(wrapper.find('a').length).toBe(1);
+    wrapper.setProps({ hide: true }); // test how components behave over time with changing props.
+    expect(wrapper.get(0)).toBeNull(); // get method simply returns the node at the given index of the current wrapper. toBeNull method from Jest to make sure that this node that we are returning is actually null.
+  });
 });
